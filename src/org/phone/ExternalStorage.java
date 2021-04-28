3c61d042476f4820ad8dab021a4a5423a80ec853
Thiru <tselvarajit@outlook.com>
2021-04-28 10:43:28 +0530

    class1 task completed

--- src/org/phone/ExternalStorage.java
+++ src/org/phone/ExternalStorage.java
@@ -1,7 +1,13 @@
 package org.phone;
 
+import javax.management.openmbean.OpenMBeanAttributeInfo;
+
 public class ExternalStorage {
 public void Size() {
 System.out.println("External Storage empty");
+}
+public static void main(String[] args) {
+	ExternalStorage ex = new ExternalStorage();
+	ex.Size();
 }
 }
